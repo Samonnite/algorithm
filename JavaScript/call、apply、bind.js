b558d6a13bc5bcde9afb7a8c7de350925a8f3b4c
Samonnite <981003163@qq.com>
2021-11-30 18:29:48 +0800

    fix: 完善bind

--- JavaScript/call、apply、bind.js
+++ JavaScript/call、apply、bind.js
@@ -50,3 +50,26 @@ Function.prototype.myBind = function (context = window, ...args1) {
     return this.call(context, ...args1, ...args2)
   }
 }
+
+Function.prototype.myBind = function(context){
+  //这里对调用者做一个判断，如果不是函数类型，直接抛异常
+  if(typeof this !== 'function'){
+    throw '调用必须为函数'
+  }
+  //当我们调用bind函数时，我们可能传了不只一个参数
+  //如 fun.bind({}, arg1, arg2)
+  //我们需要把后面的参数拿出来
+  let args = Array.prototype.slice.call(arguments, 1);
+  let fToBind = this;
+  let fNOP = function(){};
+  let fBound = function(){
+    return fToBind.apply(this instanceof fNOP ? this : context, args.concat(arguments));
+  }
+  if(this.prototype){
+    fNOP.prototype = this.prototype;
+  }
+
+  fBound.prototype = new fNOP();
+
+  return fBound;
+}

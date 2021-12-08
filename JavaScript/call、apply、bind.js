57a5beb49dd1c5a58b164f8c25c2155d7518b1d4
Samonnite <981003163@qq.com>
2021-12-08 12:16:21 +0800

    feat: 新增js

--- JavaScript/call、apply、bind.js
+++ JavaScript/call、apply、bind.js
@@ -56,26 +56,20 @@ Function.prototype.myBind = function (context = window, ...args1) {
  * 它包装了原函数对象。调用绑定函数通常会导致执行包装函数。
  * 函数内部有BoundThis属性，在调用包装函数时始终作为 this 值传递的值。
  */
-Function.prototype.myBind = function(context){
-  //这里对调用者做一个判断，如果不是函数类型，直接抛异常
-  if(typeof this !== 'function'){
-    throw '调用必须为函数'
-  }
-  //当我们调用bind函数时，我们可能传了不只一个参数
-  //如 fun.bind({}, arg1, arg2)
-  //我们需要把后面的参数拿出来
-  let args = Array.prototype.slice.call(arguments, 1);
-  let fToBind = this;
-  let fNOP = function(){};
-  let fBound = function(){
-    // 当bind后作为一个构造函数的时候，是不用改变this的指向
-    return fToBind.apply(this instanceof fNOP ? this : context, args.concat(arguments));
-  }
-  if(this.prototype){
-    fNOP.prototype = this.prototype;
+ Function.prototype.bind = function (context, ...args) {
+  if (typeof this !== "function") {
+    throw new Error("Function.prototype.bind - what is trying to be bound is not callable");
   }
 
-  fBound.prototype = new fNOP();
+  var self = this;
 
-  return fBound;
+  var fbound = function () {
+      self.apply(this instanceof self ? 
+          this : 
+          context, args.concat(Array.prototype.slice.call(arguments)));
+  }
+
+  fbound.prototype = Object.create(self.prototype);
+
+  return fbound;
 }

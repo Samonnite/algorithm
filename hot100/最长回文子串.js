a7824735ba44693da4b933b7e48df74d5d680462
Samonnite <981003163@qq.com>
2022-11-09 09:09:29 +0800

    feat: hot100 最长回文子串dp

--- hot100/最长回文子串.js
+++ hot100/最长回文子串.js
@@ -4,6 +4,7 @@
  * @param {string} s
  * @return {string}
  */
+// 中心扩散
 const longestPalindrome = function (s) {
     if (s.length < 2) {
         return s
@@ -31,3 +32,22 @@ const longestPalindrome = function (s) {
     return res
 
 };
+
+// dp
+const longestPalindromeRe = function (s) {
+    let n = s.length;
+    let res = '';
+    let dp = Array.from(new Array(n), () => new Array(n).fill(false))
+    for (let i = n - 1; i >= 0; i--) {
+        for(let j = i;j < n;j++){
+            dp[i][j] = s[i] === s[j] && (j - i < 2 || dp[i + 1][j - 1])
+            if(dp[i][j] && j - i + 1 > res.length) {
+                res = s.substring(i, j+ 1);
+            }
+        }
+    }
+    console.log(res);
+    return res;
+}
+
+longestPalindromeRe('ssasse')

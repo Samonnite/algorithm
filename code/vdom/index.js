33d609c5441c96ff02a11fff74db1aa2894094bf
Samonnite <981003163@qq.com>
2022-11-23 23:33:37 +0800

    feat: add vdom patch

--- code/vdom/index.js
+++ code/vdom/index.js
@@ -35,14 +35,106 @@ function createElementByVNode(vnode) {
 }
 
 function removeVnodes(element, children, startIndex, endIndex) {
-
+    for (const vNode of children.slice(startIndex, endIndex)) {
+        element.removeChild(vNode)
+    }
 }
 
-function addVnodes(element, children, startIndex, endIndex) { }
+function addVnodes(element, children, startIndex, endIndex) {
+    for (const vNode of children.slice(startIndex, endIndex)) {
+        element.appendChild(createElementByVNode(vNode))
+    }
+}
 
-function updateProps(element, oldVnode, newVnode) { }
+function updateProps(element, oldVnode, newVnode) {
+    function updateClass() {
+        if (oldVnode.props?.class !== newVnode.props?.class) {
+            // 对于 class 暴力解决进行更新，如果精细控制可通过 ClassList API
+            if (newVnode.props?.class) {
+                element.className = newVnode.props.class
+            } else {
+                element.className = ''
+            }
+        }
+    }
+    // 示例一:
+    // { color: 'red', fontSize: '18px' } => { backgroundColor: 'red', fontSize: '18px' }
+    //
+    // 示例二:
+    // { color: 'red', fontSize: '20px' } => { backgroundColor: 'red', fontSize: '18px' }
+    function updateStyle() {
+        const newStyle = newVnode.props?.style || {}
 
-function updateChildren(element, oldChildren, newChildren) { }
+        element.style = Object.entries(newStyle).reduce((acc, [key, value]) => {
+            return `${acc}${key.replace(/[A-Z]/g, x => '-' + x.toLowerCase())}: ${value}`
+        })
+    }
+
+    function updateAttribute() {
+        const newProps = newProps.props || {}
+
+        Object.entries(newProps).map(([key, value]) => {
+            if (key !== 'class' && key !== 'style') {
+                element.setAttribute(key, value)
+            }
+        })
+    }
+
+    updateClass()
+    updateStyle()
+    updateAttribute()
+}
+
+function updateChildren(element, oldChildren, newChildren) {
+    if (oldChildren) {
+        // 如果仅仅在旧的虚拟节点存在children，则需要在DOM中删除旧节点的所有字节点
+        removeVnodes(element, oldChildren, 0, oldChildren.length)
+        return
+    } else if (newChildren) {
+        // 如果仅仅在新的虚拟节点存在children，则需要再新的虚拟节点构建DOM并插入到element下
+        addVnodes(element, newChildren, 0, newChildren.length)
+        return
+    }
+
+    let oldVnodeIndex = 0, newVnodeIndex = 0
+    let oldVnodeEndIndex = oldChildren.length, newVnodeEndIndex = newChildren.length
+    while (oldVnodeIndex < oldVnodeEndIndex && newVnodeIndex < newVnodeEndIndex) {
+        const oldVnode = oldChildren[oldVnodeIndex]
+        const newVnode = newChildren[newVnodeEndIndex]
+
+        if (oldVnode.props.key) {
+            // 一下是旧新节点对比：
+            // oldKey: 1 2 3 4 6
+            // newKey: 4 3 5 1 2
+            // 生成 newChild 关于 key 与 index 的对应关系
+            // {4: 0, 3: 1, 5: 2, 1: 3, 2: 4}
+            const newChildrenKeyMapId = newChildren.reduce((acc, x, idx) => {
+                acc[x.key] = idx
+                return acc
+            }, {})
+            // 找到与当前旧节点 key 对应的新节点的 id
+            const id = newChildrenKeyMapId[oldVnode.props.key]
+            if (id) {
+                // 如果有相同key的新旧节点
+                patch(oldVnode, newChildren[id])
+                [newChildren[id], newChildren[newVnodeIndex]] = [newChildren[newVnodeIndex], newChildren[id]]
+                oldVnodeIndex++
+                newVnodeIndex++
+            } else {
+                // 如果在新节点中找不到与旧节点对应的key，则删掉该节点
+                // oldKey: 2 1 3 4
+                // newKey: 3 1
+                // 操作: Delete 2
+                removeNodes(element, oldChildren, oldVnodeIndex, oldVnodeIndex + 1)
+                oldVnodeIndex++
+            }
+        } else {
+            patch(oldVnode, newVnode)
+        }
+    }
+    addVnodes(element, newChildren, newVnodeIndex, newVnodeEndIndex)
+    removeVnodes(element, oldChildren, oldVnodeIndex, oldVnodeIndex)
+}
 
 function updateText(oldVnode, newVnode) {
     const element = oldVnode.element

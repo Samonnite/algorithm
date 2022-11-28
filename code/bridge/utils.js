ee6329d61d49857241dc862bec2686c268773404
Samonnite <981003163@qq.com>
2022-11-28 23:32:39 +0800

    feat(bridge): add utils

--- code/bridge/utils.js
+++ code/bridge/utils.js
@@ -1,3 +1,15 @@
-export const isIOS = () => {};
+export const isIOS = () => {
+    return [
+        'iPad Simulator',
+        'iPhone Simulator',
+        'iPod Simulator',
+        'iPad',
+        'iPhone',
+        'iPod'
+      ].includes(navigator.platform)
+      // iPad on iOS 13 detection
+      || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+};
+
 export const isAndroid = () => {};
 export const isApp = () => {};

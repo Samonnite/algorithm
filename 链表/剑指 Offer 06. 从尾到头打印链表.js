208f216959b83e8264da7cd57537aa26faf1aa59
Samonnite <981003163@qq.com>
2022-12-02 20:00:51 +0800

    feat: 剑指 Offer 06. 从尾到头打印链表

---
+++ 链表/剑指 Offer 06. 从尾到头打印链表.js
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number[]}
+ */
+var reversePrint = function (head) {
+    if (!head) return [];
+    let res = reversePrint(head.next);
+    res.push(head.val);
+    return res;
+}

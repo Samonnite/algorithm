b7f38b1b371f9bf9310d16742e47834174d58b5e
Samonnite <981003163@qq.com>
2022-12-03 21:42:19 +0800

    feat: 20. Valid Parentheses

---
+++ category/stack/20. Valid Parentheses.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    const stack = [];
+    for (let i = 0; i < s.length; i++) {
+        let c = s[i];
+        switch (c) {
+            case '{':
+                stack.push('}');
+                break;
+            case '[':
+                stack.push(']');
+                break;
+            case '(':
+                stack.push(')');
+                break;
+            default:
+                if (c !== stack.pop()) return false;
+        }
+    }
+    return stack.length === 0;
+}
